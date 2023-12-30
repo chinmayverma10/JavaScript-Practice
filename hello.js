6b3a8b6911df76743655ac39761d546cd6f3ab6f
codeslap <chinmay.codesworld@gmail.com>
2023-12-31 01:23:54 +0530

    FirstChapterComplete

--- hello.js
+++ hello.js
@@ -1,4 +1,30 @@
+//CHAPTER --> 1
+
 console.log("Hello World");
-alert("hello")
-name = "Chinmay"
-console.log(name)
+fullName = "Chinmay";
+console.log(fullName);
+let name = "Chinmay";
+var age = 18;
+const year = 50;
+console.log(typeof year)
+
+const student = {
+    fullName : 'Chinmay',
+    age : 20,
+    cgps : 7.7
+};
+console.log(student);
+
+
+//Practice question on objects
+const socialMedia = {
+    username : 'chinmayverma',
+    isFollow : true,
+    followers : 350,
+    following : 100
+};
+console.log(socialMedia);
+
+
+
+//CHAPTER --> 2
