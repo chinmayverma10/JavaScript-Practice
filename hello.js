bdedd90bdfb167acfe1c2e68a6ff21e6df2829e8
codeslap <chinmay.codesworld@gmail.com>
2023-12-31 17:09:59 +0530

    Chapter2Commit

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ console.log("Hello World");
 fullName = "Chinmay";
 console.log(fullName);
 let name = "Chinmay";
-var age = 18;
+// var age = 18;
 const year = 50;
 console.log(typeof year)
 
@@ -26,5 +26,39 @@ const socialMedia = {
 console.log(socialMedia);
 
 
-
 //CHAPTER --> 2
+
+//This is single line comment
+/*Thus is multi line comment*/
+
+//Conditional-Statements
+let color;
+let mode = 'dark-mode';
+if(mode === "dark-mode"){
+    color = "black";
+}
+console.log(color)
+
+let age = 15;
+age >= 18 ? console.log('adult') : console.log('young');
+
+
+
+//Practice ques
+// let num = prompt("Enter a number");
+// num%5 === 0 ? console.log("yes") : console.log("no");
+
+
+let marks = prompt("Enter marks");
+
+if(marks>=90 && marks<=100){
+    console.log("A");
+}else if(marks>=80 && marks<=89){
+    console.log("B");
+}else if(marks>=70 && marks<=79){
+    console.log("C");
+}else if(marks>=60 && marks<=69){
+    console.log("D");
+}else {
+    console.log("F");
+}
