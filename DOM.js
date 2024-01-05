a3baeff4c8f02bfb32848147c32ab787bb881892
codeslap <chinmay.codesworld@gmail.com>
2024-01-05 09:01:22 +0530

    Chapter6 Commit

--- DOM.js
+++ DOM.js
@@ -1,3 +1,17 @@
 console.dir(document)
 console.log(document)
-let x = document.getElementsByTagName("h3");
+
+document.querySelector("html") //--> Will return conplete html code
+document.querySelector("html").children[1]; //--> Will return body tag
+
+
+let div = document.querySelector("div");
+console.log(div);
+// div.innerText = "Dragon Fruit";
+console.log(div.innerText);
+console.log(div.innerHTML);
+console.log(div.tagName);
+console.log(div.textContent);
+
+let h2 = document.querySelector(".hellonew");
+h2.innerText = h2.innerText + " from apna college students";
